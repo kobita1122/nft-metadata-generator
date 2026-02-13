688aa365ac289fc8bfd7cb2bc5b6f5d4edb11cb6
Kobita <161524322+kobita1122@users.noreply.github.com>
2026-02-13 14:03:36 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  baseUri: "ipfs://QmYourCollectionCID",
+  namePrefix: "CryptoAlpha #",
+  description: "A unique digital collectible on the blockchain.",
+  collectionSize: 10,
+  traits: ["Background", "Skin", "Eyes", "Mouth"]
+};
